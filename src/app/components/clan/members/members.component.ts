5a835b8e99ad39500adf8069a7cea042259c5b97
PauMB <paumb85@gmail.com>
2018-08-26 23:31:33 +0200

    create members component

--- src/app/components/clan/members/members.component.ts
+++ src/app/components/clan/members/members.component.ts
@@ -1,8 +1,9 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
 import {ClansService} from '../../../service/clans/clans.service';
 import {Observable} from 'rxjs';
 import {AppError} from '../../../commons/errors/app-error';
 import {HttpParams} from '@angular/common/http';
+import {MatPaginator, MatTableDataSource} from '@angular/material';
 
 @Component({
   selector: 'app-members',
@@ -12,23 +13,35 @@ import {HttpParams} from '@angular/common/http';
 export class MembersComponent implements OnInit {
 
   @Input() tagClan: string;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
   body: any;
   limit: string;
+  before: string;
+  after: string;
+  dataSource: MatTableDataSource<any>;
   params: HttpParams;
+
+  maxRowsTable: number;
+  displayedColumns: string[] = ['tag', 'name', 'role', 'expLevel', 'trophies', 'donations', 'arena'];
+  pageSizeOptions: number[] = [5, 10, 25, 50];
   constructor(private clansService: ClansService) {
-    this.limit = '10';
+    this.limit = '50';
+    this.before = '';
+    this.after = '';
   }
 
   ngOnInit() {
+    this.maxRowsTable = 10;
     this.getMembers();
   }
 
   private getMembers() {
-    this.params = new HttpParams().set('limit', this.limit);
+    this.params = this.setParams();
     this.clansService.getMembersClan(this.tagClan, this.params).subscribe(
       response => {
         this.body = response.body;
-        console.log(this.body);
+        this.dataSource = new MatTableDataSource(this.body.items);
+        this.dataSource.paginator = this.paginator;
       },
       error => this.handlerError(error)
     );
@@ -36,5 +49,16 @@ export class MembersComponent implements OnInit {
 
   private handlerError(err: Response) {
     return Observable.create(new AppError(err));
+  }
+
+  private setParams() {
+    const myParams = new HttpParams().set('limit', this.limit);
+    if (this.before !== '') {
+      myParams.set('before', this.before);
+    }
+    if (this.after !== '') {
+      myParams.set('after', this.after);
+    }
+    return myParams;
   }
 }
